fae548e022e80a9104434eeb583ee7c5e191909a
Mohammad Fareed <fareed543@gmail.com>
2022-02-04 11:16:14 +0530

    Flutter App

---
+++ android/app/src/main/kotlin/com/example/bhc/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.bhc
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+// Copyright 2018 The Flutter team. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Welcome to Flutter',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('Welcome to Flutter'),
+        ),
+        // body: const WebView(
+        //   javascriptMode: JavascriptMode.unrestricted,
+        //   initialUrl: 'https://fareed.in',
+        // ),
+        body: const Center(
+          child: Text('Hello World'),
+        ),
+      ),
+    );
+  }
+}
